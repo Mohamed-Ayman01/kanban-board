1e9f14163dfec146847aa1a4ef19086f46a825e2
Mohamed-Ayman01 <lord39597@gmail.com>
2023-09-10 17:23:48 +0400

    added toggle between projects, in valid project name notify

--- main.js
+++ main.js
@@ -1,10 +1,54 @@
-//* check if there are any saved tabs
-
+//! check if there are any saved tabs
 window.addEventListener("load", checkForTabs);
+
+function appendTasksFrom(currentProjectP) {
+  let projectsData = JSON.parse(localStorage.getItem("projects-tabs"));
+
+  for (obj of projectsData) {
+    if (obj.name === currentProjectP.textContent) {
+      let todoCont = document.querySelector(".board .todo-sect .task-cont");
+      let inProgressCont = document.querySelector(".board .in-progress-sect .task-cont");
+      let completedCont = document.querySelector(".board .completed-sect .task-cont");
+
+      todoCont.innerHTML = "";
+      inProgressCont.innerHTML = "";
+      completedCont.innerHTML = "";
+
+      obj.tasks.forEach((task) => {
+        let taskBox = document.createElement("div");
+        taskBox.classList.add("task");
+
+        let p = document.createElement("p");
+        p.textContent = task.value;
+
+        let button = document.createElement("button");
+        button.classList.add("edit-task");
+
+        let buttonIco = document.createElement("i");
+        buttonIco.classList.add("fas", "fa-wrench");
+
+        button.append(buttonIco);
+
+        taskBox.append(p, button);
+
+        if (task.status === 1) {
+          todoCont.append(taskBox);
+        } else if (task.status === 2) {
+          inProgressCont.append(taskBox);
+        } else if (task.status === 3) {
+          completedCont.append(taskBox);
+        }
+
+      });
+    }
+  }
+
+  localStorage.setItem("projects-tabs", JSON.stringify(projectsData));
+}
 
 function appendTabsFrom(projectsArr) {
   let projectsBox = document.querySelector(
-    "aside .projects-control .projects-box"
+    "aside .projects-control .projects-box",
   );
   projectsBox.innerHTML = "";
 
@@ -26,6 +70,20 @@ function appendTabsFrom(projectsArr) {
 
     projectsBox.append(proj);
   });
+
+  let allProjects = document.querySelectorAll(".projects-box .proj");
+
+  allProjects.forEach((proj) => {
+    if (!proj.classList.contains("active")) return;
+
+    let projChilds = Array.from(proj.children);
+
+    projChilds.forEach((child) => {
+      if (child.tagName === "P") {
+        appendTasksFrom(child);
+      }
+    });
+  });
 }
 
 function checkForTabs() {
@@ -36,7 +94,10 @@ function checkForTabs() {
       {
         name: "project 1",
         isActive: true,
-        tasks: [],
+        tasks: [
+          { value: Math.random(), status: 2 },
+          { value: Math.random(), status: 3 },
+        ],
       },
     ];
 
@@ -57,17 +118,18 @@ function notifyWith(message) {
 
   setTimeout(() => {
     popUp2.style.top = "10px";
-  }, 500)
-  
+  }, 500);
+
   setTimeout(() => {
     popUp2.style.top = "-80px";
-  }, 3000)
+  }, 3000);
 
   setTimeout(() => {
     popUp2.remove();
-  }, 5000)
+  }, 5000);
 }
 
+//! Add new project controls
 let addProjectInput = document.querySelector("aside input#proj-input");
 let addProjectBtn = document.querySelector("aside .controls button");
 
@@ -76,13 +138,57 @@ addProjectBtn.addEventListener("click", () => {
 
   let inputValue = addProjectInput.value;
   let inputValueValid = inputValue !== "" ? true : false;
+
   addProjectInput.value = "";
 
+  for (obj of projects) {
+    if (obj.name === inputValue) return notifyWith("this project already exsists")
+  }
+
+
   if (inputValueValid) {
-    projects.push({ name: inputValue, isActive: false, tasks: [] });
+    projects.push({
+      name: inputValue,
+      isActive: false,
+      tasks: [
+        { value: Math.random(), status: 2 },
+        { value: Math.random(), status: 3 },
+      ],
+    });
 
     localStorage.setItem("projects-tabs", JSON.stringify(projects));
 
     appendTabsFrom(JSON.parse(localStorage.getItem("projects-tabs")));
-  } else notifyWith("field is empty")
+  } else notifyWith("field is empty");
+});
+
+//! Toggle between projects tabs
+window.addEventListener("click", (e) => {
+  let currentEl = e.target;
+  let currentElParent = currentEl.parentNode;
+
+  if (currentEl.tagName === "BUTTON") return;
+  if (!currentElParent.classList.contains("proj")) return;
+
+  let allProjects = document.querySelectorAll(".projects-box .proj");
+
+  allProjects.forEach((proj) => {
+    proj.classList.remove("active");
+  });
+
+  currentElParent.classList.add("active");
+
+  let projectsData = JSON.parse(localStorage.getItem("projects-tabs"));
+
+  for (obj of projectsData) {
+    if (obj.name === currentEl.textContent) {
+      obj.isActive = true;
+    } else {
+      obj.isActive = false;
+    }
+  }
+
+  localStorage.setItem("projects-tabs", JSON.stringify(projectsData));
+
+  appendTasksFrom(currentEl);
 });
